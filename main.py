ef5954199d35b9cd6308cdbaa8576f048603a995
dima <test@dima.org>
2022-06-26 14:35:50 +0300

    функция сравнения и потом добавить новую функцию фильтрации

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
+from codecs import escape_encode
+from doctest import Example
 import random
+
+# сравнивает строки
+def text_match(user_text, example):
+    # приводим к нужному регистру
+    example = example.lower()
+    user_text = user_text.lower()
+
+    # теперь еще нужно в конце спец.символы убрать (Привет == Привет!!!)
+    # можно сделать с помощью регулярных выражений - удалить в строке все сзнаки припинания
+    
+    return user_text == example
+
+# фильтрация = очистка текста
+def filter_text(text):
+    text = text.lower()
+    return text
 
 text = input()
 if text in ['Hi', "hello"]:
